2df58052699cdf587ba31bbdf2ff9146a07b679a
Ashley Lee <ashley.lee@ubc.ca>
2022-04-07 13:21:20 -0700

    UI changes
    UI changes

--- index.php
+++ index.php
@@ -387,7 +387,10 @@ $Proj = new Project();
                                         print "[" . $destInstrEvent . "]";
                                         print "<input class='destInstrEvents' type='hidden' name='destInstrEvents[$index][]' value='" . $destInstrEvent . "'>";
                                     }
-                                    print "[" . $source . "]";
+                                    else 
+                                    {
+                                        print "<i>The first event of the destination project</i>"
+                                    }
                                     print "</td><td><span class='fa fa-pencil-alt' onclick='fillInstrForm(this)'></span></td>";
                                     print "<td><span class='fa fa-trash-alt delete-trigger-field'></span></td>";
                                     print "</tr>";
