b446c7410b432bf00098b16b35b61770a4253402
Ashley Lee <ashley.lee@ubc.ca>
2022-05-04 11:18:16 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -204,8 +204,8 @@ function addTrigger()
                 "</table>" +
                 "<h6>Generate Survey URLs (Optional)</h6>" + 
                 "<p>If specified, the destination project will generate a survey url for the participant to redirect to. If at least one trigger is met, a survey url will generate.</p>" + 
-                "<div class='row'>"
-                    "<div class='form-check col-6'>"
+                "<div class='row'>" + 
+                    "<div class='form-check col-6'>" + 
                         "<label>Specify the destination instrument the module will generate a survey url from.</label>" + 
                         "<div class='row'>" + 
                             "<div class='col-sm-6 ui-front dest-event-wrapper'>" +
