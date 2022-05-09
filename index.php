324b449fd0dca539ed069d7e38adb7eedd6fbedd
Ashley Lee <ashley.lee@ubc.ca>
2022-05-09 16:04:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -443,9 +443,9 @@ $Proj = new Project();
                         <h6>Confirm the following</h6>
                         <div class='row form-group'> 
                             <div class="col-sm-6">
-                                <div class="col-sm-12"><label>Overwrite data in destination project every time data is saved? This determines whether to push blank data over to the destination project.</label></div>
-                                <?php if ($trigger_obj["overwrite-data"] == "overwrite"):?>
+                                <label>Overwrite data in destination project every time data is saved? This determines whether to push blank data over to the destination project.</label>
                                 <div class="form-check col-sm-12">
+                                    <?php if ($trigger_obj["overwrite-data"] == "overwrite"):?>
                                     <input type="radio" name="triggers[<?php print $index;?>][overwrite-data]" class="form-check-input" value="overwrite" checked required><label class="form-check-label">Yes</label>
                                     <br>
                                     <input type="radio" name="triggers[<?php print $index;?>][overwrite-data]" class="form-check-input" value="normal" required><label class="form-check-label">No</label>
@@ -457,9 +457,9 @@ $Proj = new Project();
                                 </div>
                             </div>
                             <div class='col-sm-6'>
-                                <div class="col-sm-12"><label>Import data access groups (DAGs) every time data is saved? The module can only import DAGs if they have a one-to-one relationship with the destination project.</label></div>
-                                <?php if ($trigger_obj["import-dags"] == "1"):?>
+                                <label>Import data access groups (DAGs) every time data is saved? The module can only import DAGs if they have a one-to-one relationship with the destination project.</label>
                                 <div class="form-check col-sm-12">
+                                    <?php if ($trigger_obj["import-dags"] == "1"):?>
                                     <input type="radio" name="triggers[<?php print $index;?>][import-dags]" class="form-check-input" value="1" checked required><label class="form-check-label">Yes</label>
                                     <br>
                                     <input type="radio" name="triggers[<?php print $index;?>][import-dags]" class="form-check-input" value="0" required><label class="form-check-label">No</label>
@@ -467,7 +467,7 @@ $Proj = new Project();
                                     <input type="radio" name="triggers[<?php print $index;?>][import-dags]" class="form-check-input" value="1" required><label class="form-check-label">Yes</label>
                                     <br>
                                     <input type="radio" name="triggers[<?php print $index;?>][import-dags]" class="form-check-input" value="0" checked required><label class="form-check-label">No</label>
-                                <?php endif; ?>
+                                    <?php endif; ?>
                                 </div>
                             </div>
                         </div>
