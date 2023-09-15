eb1f0879cfd473bf4c38bea8c7cd2ae821d46052
Ashley Lee <ashley.lee@ubc.ca>
2023-09-15 09:11:42 -0700

    Update index.php
    Updated CDN for bootstrap-select

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ if ($settings == null)
 <html>
     <head>
         <!-- boostrap-select css and js-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.15/dist/css/bootstrap-select.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.15/dist/js/bootstrap-select.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
         <style>
             p {
                 max-width: 100%;
